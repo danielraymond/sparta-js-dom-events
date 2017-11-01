81e31976727bdbd046a9bcaf574f87753dc5f96c
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-01 16:56:29 +0000

    finished dom events lesson

--- dom-events-starter-code/index.js
+++ dom-events-starter-code/index.js
@@ -1,2 +1,42 @@
-// write code here
- 
+// add top line to all js code
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  console.log("the content is loaded")
+  // write code here
+
+  var myButton = document.getElementById('myButton');
+
+  // First param - type of action
+  //  Second param - what will happen (callback)
+  // myButton.addEventListener("click", function() {
+  //
+  //    console.log("Button was clicked");
+  //
+  // })
+
+  myButton.addEventListener("click", handleClick);
+
+  function handleClick() {
+    console.log("Button was clicked via a function");
+
+  }
+
+  var myForm = document.getElementById('myForm');
+
+  myForm.addEventListener('submit', function(event){
+    console.log("Form was submitted");
+    event.preventDefault();
+  })
+
+  var buttons = document.querySelectorAll('.myButtons');
+
+  for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener("click", function(event){
+      console.log(this.value + " was clicked");
+    })
+  }
+
+// end of the top function
+})
